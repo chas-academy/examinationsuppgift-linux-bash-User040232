a7e679c8e4139af2413b8378c2f00189de29a126
User040232 <stan02x@icloud.com>
2026-04-03 00:27:06 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,38 +1,50 @@
-#!/bin/bash   - (#) script som skapar användare och mappar 
+#!/bin/bash   
+# Script som skapar användare, mappar och en välkomstfil
 
-# kontrollera så att scriptet körs som root
-if [ $EUID -ne 0 ]; then
-echo "måste köra scriptet som root!" 
-exit 
+# Kontrollera så att scriptet körs som root
+if [ "$EUID" -ne 0 ]; then
+  echo "Du måste köra scriptet som root!" 
+  exit 1
 fi
 
-# gå igenom alla användare som skickas in 
+# Kontrollera att minst en användare skickas in 
+if [ "$#" -eq 0 ]; then 
+  echo "Användning: $0 användare1 användare2 ..."
+  exit 1
+fi
+
+# Loopa igenom alla användare 
 for user in "$@"
 do
+    # Skapa användare med hemkatalog om den inte finns 
+    if id "$user" &>/dev/null; then
+        echo "Användaren $user finns redan, hoppar över ..."
+        continue
+    fi
 
-# skapa användare med hemkatalog useradd -m $user
+    useradd -m "$user"
 
-# skapa mappar
-mkdir /home/$user/Documents
-mkdir /home/$user/Downloads
-mkdir /home/$user/Work
+    # Skapa mappar
+    mkdir -p /home/"$user"/Documents
+    mkdir -p /home/"$user"/Downloads
+    mkdir -p /home/"$user"/Work
 
-# ändra ägare till användaren
-chown -R $user:$user /home/$user
+    # Sätt ägare
+    chown -R "$user":"$user" /home/"$user"
 
-# lägger rättigheter så bara ägaren kan läsa/skriva 
-chmod 700 /home/$user/Documents
-chmod 700 /home/$user/Downloads
-chmod 700 /home/$user/Work
+    # Sätt rättigheter (endast ägaren) 
+    chmod 700 /home/"$user"/Documents
+    chmod 700 /home/"$user"/Downloads
+    chmod 700 /home/"$user"/Work
 
-# skapa welcome fil 
-echo "Välkommen $user" > /home/$user/welcome.txt
-echo "Andra användare i systemet:" >> home/$user/welcome.txt
+    # Skapa welcome fil 
+    echo "Välkommen $user" > /home/"$user"/welcome.txt
+    echo "Andra användare i systemet:" >> home/"$user"/welcome.txt
 
-# lista användare 
-cut -d: -f1 /etc/passwd >> /home/$user/welcome.txt
+    # Lista riktiga användare (UID >= 1000)
+    awk -F: '$3 >= 1000 {print $1}' /etc/passwd >> /home/"$user"/welcome.txt
 
-# sätta rätt ägare på filen 
-chown $user:$user /home/$user/welcome.txt
+    # Sätt rätt ägare på filen 
+    chown "$user":"$user" /home/"$user"/welcome.txt
 
 done
